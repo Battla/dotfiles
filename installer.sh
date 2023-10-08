60d4f7cf173edfda334e3c65eca401dcdc99a2ac
Battla <58558903+Battla@users.noreply.github.com>
2023-10-08 19:35:53 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 #declaring every pkg that I want to installed
 declare -a pkgToInstalled=("git" "curl" "net-tools" "tmux" "pkg-config")
 
@@ -12,20 +11,25 @@ for i in "${pkgToInstalled[@]}"; do
     echo "$i"
 done
 
-nvm=$(command -v nvm)
+#echo "$distro , $architecture, $version"
 
-echo "$distro , $architecture, $version"
-
-if ! $nvm >/dev/null 2>&1; then
-   echo "nvm was not found"
-   # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-   #. ~/.bashrc
-   # if ! $nvm >/dev/null 2>&1; then
-   #
-   # nvm install --lts
-   # nvm use --lts
-   #fi
-else
-    
+if command -v nvm >/dev/null 2>&1; then
     echo "nvm was found"
+    node --version
+    npm -v
+
+else
+    echo "nvm was not found"
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"^
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    command -v nvm
+    #. ~/.bashrc
+    # if command -v nvm >/dev/null 2>&1; then
+    #
+    # nvm install --lts
+    # nvm use --lts
+    #fi
+
 fi
