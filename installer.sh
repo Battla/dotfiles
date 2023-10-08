028d8f6471a2e34aa60be20a13dc933a477ede45
Battla <58558903+Battla@users.noreply.github.com>
2023-10-08 19:37:42 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -21,7 +21,7 @@ if command -v nvm >/dev/null 2>&1; then
 else
     echo "nvm was not found"
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"^
+    export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     command -v nvm
